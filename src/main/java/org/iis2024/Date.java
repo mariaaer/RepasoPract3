03f72c482fd7af5b7709d77647c2bdaa95192533
BorjaCuenca <borjacpaez11@gmail.com>
2024-04-16 16:11:43 +0200

    ´qEHIGẃi

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -57,10 +57,10 @@ public class Date {
    * @param year The year (should ideally be within the valid range of 1900 to 2050 for validation
    *     to pass).
    */
-  public Date(int adios, int monat, int jahr) {
-	  this.day = adios;
-	  this.month = monat;
-	  this.year = jahr;
+  public Date(int day, int month, int year) {
+	  this.day = day;
+	  this.month = month;
+	  this.year = year;
 	 } 
 
   /**
